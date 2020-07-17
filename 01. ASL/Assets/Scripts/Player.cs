44fbc1bbfeeee04cf5127ddf011ea69ecd2d664f
jtarrier-sc <jtarrier@yahoo.com>
2020-07-17 16:46:52 +0800

    'Player' C# script - Added challenge outline.

--- 01. ASL/Assets/Scripts/Player.cs
+++ 01. ASL/Assets/Scripts/Player.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    /// <summary>
+    /// Create a program that allows you to enter your age, sex, and location into
+    /// the inspector. When you press the space key, it should print out the values.
+    /// You should be able to change these values at runtime. Example Print: "25 M Atlanta"
+    /// </summary>
+    
+
     // Start is called before the first frame update
     void Start()
     {
